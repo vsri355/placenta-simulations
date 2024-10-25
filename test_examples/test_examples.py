a4f6ce3a953b84b32d7c29da0546b09754ad6c2f
Alys Clark <acla148@bn405952.uoa.auckland.ac.nz>
2024-10-25 13:35:09 +1300

    os agnostic removal of files

--- test_examples/test_examples.py
+++ test_examples/test_examples.py
@@ -6,24 +6,23 @@
 # necessary, and where possible write and use simple testing on the lungsim libraries to fully test your changes to
 # the code. This is not a unit test, its a large-scale simulation test.
 
-import os
+import os,shutil
 import compare_results as cr
 
 def main():
     export_directory = 'output'
-
-    if not os.path.exists(export_directory):
-        os.makedirs(export_directory)
-
     everything_worked = True
     mydir = os.getcwd() # would be the same path as a.py
     export_logs_dir = mydir + '/' +  export_directory + '/'
+    if not os.path.exists(export_logs_dir):
+        os.makedirs(export_logs_dir)
     #--------------------------------------------------
     #Test  - Grow tree
     #-------------------------------------------------
     mydir_example1 = os.chdir("../geometry/grow-tree-ellipsoid")
     mydir_example1 =os.getcwd()
-    os.system('rm -rf output/*') #remove any files in the output directory
+    if os.path.exists(export_directory):
+        shutil.rmtree(export_directory) #remove any files in the output directory
     os.system('python grow-tree.py > '+ export_logs_dir + 'grow-tree.log') #run the model -> export
     # terminal output to log file
     os.chdir(mydir)
@@ -45,7 +44,8 @@ def main():
     # -------------------------------------------------
     mydir_example1 = os.chdir("../geometry/grow-tree-fgr")
     mydir_example1 = os.getcwd()
-    os.system('rm -rf output/*')  # remove any files in the output directory
+    if os.path.exists(export_directory):
+        shutil.rmtree(export_directory) #remove any files in the output directory
     os.system('python grow-tree.py > ' + export_logs_dir + 'grow-tree.log')  # run the model -> export
     # terminal output to log file
     os.chdir(mydir)
@@ -67,7 +67,8 @@ def main():
     #-------------------------------------------------
     mydir_example1 = os.chdir("../geometry/grow-tree-cuboid")
     mydir_example1 =os.getcwd()
-    os.system('rm -rf output/*') #remove any files in the output directory
+    if os.path.exists(export_directory):
+        shutil.rmtree(export_directory)  # remove any files in the output directory
     os.system('python grow-tree.py > '+ export_logs_dir + 'grow-tree.log') #run the model -> export
     # terminal output to log file
     os.chdir(mydir)
@@ -85,7 +86,8 @@ def main():
     #-------------------------------------------------
     mydir_example1 = os.chdir("../fetoplacental/interface2015")
     mydir_example1 =os.getcwd()
-    os.system('rm -rf output/*') #remove any files in the output directory
+    if os.path.exists(export_directory):
+        shutil.rmtree(export_directory)  # remove any files in the output directory
     os.system('python bloodflow_interface2015.py > '+ export_logs_dir + 'clark2015.log') #run the model -> export
     # terminal output to log file
     os.chdir(mydir)
@@ -101,7 +103,8 @@ def main():
     #-------------------------------------------------
     mydir_example1 = os.chdir("../fetoplacental/two_umb_arteries")
     mydir_example1 =os.getcwd()
-    os.system('rm -rf output/*') #remove any files in the output directory
+    if os.path.exists(export_directory):
+        shutil.rmtree(export_directory)  # remove any files in the output directory
     os.system('python bloodflow_two_umb_arteries.py > '+ export_logs_dir + 'byrne2020.log') #run the model -> export
     os.chdir(mydir)
     # terminal output to log file
